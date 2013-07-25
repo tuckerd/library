642964cb3ec4b65f808cf4507390b80a3e53bd79
D Tucker <danielle@tuckerlabs.com>
2013-07-24 17:25:15 -0700

    Added new view for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,19 @@
 class MoviesController < ApplicationController
   def index
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(params[:movie])
+    if @movie.valid?
+      flash[:notice] = "Movie has been added to your library."
+      redirect_to @movies
+    else
+      flash[:alert] = "Movie has not been added to you library."
+      render action: 'new'
+    end
+  end
 end
--- spec/features/movies/creating_movies_spec.rb
+++ spec/features/movies/creating_movies_spec.rb
@@ -5,15 +5,16 @@ feature "Adding movies" do
     visit '/'
     click_link "New Movie"
     fill_in "Title", with: "The West Wing Season 3"
-    fill_in "Format", with: "DVD"
-    click_link "Create Movie"
+    page.select("DVD", from: "movie_format")
+      #fill_in "Format", with: "DVD"
+    click_button "Create Movie"
     page.should have_content("Movie has been added to your library.")
   end
 
   scenario "cannot create movie without a title or format" do
     visit '/'
     click_link "New Movie"
-    click_link "Create Movie"
+    click_button "Create Movie"
     page.should have_content("Movie has not been added to your library.")
     page.should have_content("Title can't be blank")
     page.should have_content("Format can't be blank")
